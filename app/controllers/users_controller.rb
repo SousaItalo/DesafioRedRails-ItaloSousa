360b426c7a0249ce92989383687c9ddd17008001
SousaItalo <italops94@gmail.com>
2017-03-21 15:26:58 -0300

    Login from view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Hey #{@user.name}, welcome to Airbnb-test! :D"
+			redirect_to signup_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :username, :email, :password)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 	
 	validates :username, presence: true, 
 						uniqueness: { case_sensitive: false }, 
-						length: {minimum: 4, maximum: 9}
+						length: {minimum: 4, maximum: 12}
 	
 	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z][a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, 
